193083dfa94a5f3f9152b88009c139788af61fdb
Kran2288 <ashirov.ayrat@gmail.com>
2020-07-05 20:15:00 +0300

    Head1

---
+++ script.js
@@ -0,0 +1,12 @@
+function objects(title, description, time, time_of_work, name, tag, data, id){
+    return{
+      title, description, time, time_of_work, name, tag, data, id
+    }
+  }
+  
+  const plan_array = [
+    // objects("Task 1", "description", "time", "time_of_work", "name", "plan"),
+    // objects("Task 2", "description", "time", "time_of_work", "name", "in-work"),
+    // objects("Task 3", "description", "time", "time_of_work", "name", "in-work"),
+    // objects("Task 4", "description", "time", "time_of_work", "name", "ended")
+  ]
